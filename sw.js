73ca200f64bc73d483346db2ac53d243389e5f0f
Alex <80994030+ThienLu@users.noreply.github.com>
2025-04-05 15:40:23 +1300

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,9 +10,9 @@ self.addEventListener('message', e => {
   if (e.data.type === 'checkTime') {
     const { currentHour, currentMin } = e.data;
 
-    if (parseInt(currentHour) === 23 && currentMin < 1) {
+    if (parseInt(currentHour) === 22 && currentMin === 30) {
       self.registration.showNotification("⚠️ Vana'diel Alert", {
-        body: "It's 23:00 Vana'diel time!",
+        body: "It's 22:30 Vana'diel time!",
         icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Circle-icons-alarm.svg/1024px-Circle-icons-alarm.svg.png"
       });
     }
