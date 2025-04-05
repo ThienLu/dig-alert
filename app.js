fd33a0346929fecaa86cf95563c5e5917af3ed22
Alex <80994030+ThienLu@users.noreply.github.com>
2025-04-05 15:39:39 +1300

    Update app.js

--- app.js
+++ app.js
@@ -17,15 +17,15 @@ function requestPermission() {
 }
 
 // Vana'diel time calc
-const msGameDay = (24 * 60 * 60 * 1000) / 25;
+const msRealDay = 24 * 60 * 60 * 1000; // milliseconds in a real day
 const basisDate = new Date(Date.UTC(2002, 5, 23, 15, 0, 0));
 
 function updateVanaTime() {
   const now = new Date();
-  const vanaMs = ((898 * 360 + 30) * 86400000) + (now.getTime() - basisDate.getTime()) * 25;
-  const vHour = Math.floor((vanaMs % 86400000) / 3600000);
-  const vMin = Math.floor((vanaMs % 3600000) / 60000);
-  document.getElementById("vanaTime").textContent = `Time: ${String(vHour).padStart(2, '0')}:${String(vMin).padStart(2, '0')}`;
+  const vanaDate = ((898 * 360 + 30) * msRealDay) + (now.getTime() - basisDate.getTime()) * 25;
+
+  const vHour = Math.floor((vanaDate % msRealDay) / (60 * 60 * 1000));
+  const vMin = Math.floor((vanaDate % (60 * 60 * 1000)) / (60 * 1000));
 
   // Send next notification when it's time
   if (navigator.serviceWorker && navigator.serviceWorker.controller) {
